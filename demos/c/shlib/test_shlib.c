8d9a8168bb6bc87949beb7aecebbd0ffb13d9c4e
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-07-12 17:12:47 +0000

    docs

--- demos/c/shlib/test_shlib.c
+++ demos/c/shlib/test_shlib.c
@@ -1,12 +1,4 @@
 
-#ifdef _MSC_VER
-#	define test_EXPORT __declspec(dllexport)
-#else
-#	define test_EXPORT
-#endif
-
-extern test_EXPORT void foo() { }
-
 static const int truc=5;
 
 void foo() { }
