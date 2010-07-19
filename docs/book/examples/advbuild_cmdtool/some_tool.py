fa5d59c3d5a2d5976e93770ea8e6f8ae0e79533f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-07-19 18:01:54 +0000

    docs

--- docs/book/examples/advbuild_cmdtool/some_tool.py
+++ docs/book/examples/advbuild_cmdtool/some_tool.py
@@ -1,24 +1,10 @@
 #! /usr/bin/env python
 
-import os
-from waflib import Utils, ConfigSet, Options, Build, Context
+from waflib import Context
 
 def cnt(ctx):
 	"""do something"""
-	print(2)
+	print('just a test')
 
 Context.g_module.__dict__['cnt'] = cnt
 
-class cnt(Build.BuildContext):
-	cmd = 'cnt'
-	fun = 'build'
-
-	def execute(self):
-		self.load()
-		if not self.all_envs:
-			self.load_envs()
-		self.recurse([self.run_dir])
-		tot = 0
-		for x in self.groups:
-			tot += len(x)
-		self.to_log('there are %d task generators in this project\n' % tot)
