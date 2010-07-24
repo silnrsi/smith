c9eb8f7949ebd505fa2fe19e1ac58390fca056b2
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-07-24 08:05:03 +0000

    docs

---
+++ docs/book/examples/scenarios_compiler/src2cpp.py
@@ -0,0 +1,16 @@
+from waflib.Task import Task
+class src2cpp(Task):
+	run_str = '${COMP} ${SRC} ${TGT}'
+	color   = 'PINK'
+
+from waflib.TaskGen import extension
+
+@extension('.src')
+def process_src(self, node):
+	tg = self.bld.get_tg_by_name('comp')
+	tg.post()
+
+	tsk = self.create_task('src2cpp', node, node.change_ext('.cpp'))
+
+	tsk.env.COMP = tg.link_task.outputs[0].abspath()
+
