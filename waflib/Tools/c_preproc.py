9b25bde4915875ebaa851ab811e14b51c55acb75
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-08-28 09:38:06 +0000

    optimization

--- waflib/Tools/c_preproc.py
+++ waflib/Tools/c_preproc.py
@@ -616,13 +616,13 @@ class c_parser(object):
 		except:
 			nd = node.ctx.cache_nd = {}
 
-		tup = hash((id(node), filename))
-		if tup in nd:
+		tup = (node, filename)
+		try:
 			return nd[tup]
-
-		ret = node.find_resource(filename)
-		nd[tup] = ret
-		return ret
+		except KeyError:
+			ret = node.find_resource(filename)
+			nd[tup] = ret
+			return ret
 
 	def tryfind(self, filename):
 		self.curfile = filename
