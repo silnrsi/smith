e603112ba7b979e9e77db8d3b9f25b607eeabef7
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-08-30 11:34:07 +0000

    check for typos

--- waflib/Tools/errcheck.py
+++ waflib/Tools/errcheck.py
@@ -6,8 +6,39 @@
 Search for common mistakes
 """
 
+typos = {
+'feature':'features',
+'sources':'source',
+'targets':'target',
+'include':'includes',
+'define':'defines',
+'importpath':'includes',
+'installpath':'install_path',
+}
+
+meths_typos = ['__call__', 'program', 'shlib', 'stlib', 'objects']
+
+from waflib import Logs, Build
+import waflib.Tools.ccroot
+
+def replace(m):
+	oldcall = getattr(Build.BuildContext, m)
+	def call(self, *k, **kw):
+		for x in typos:
+			if x in kw:
+				kw[typos[x]] = kw[x]
+				del kw[x]
+				Logs.warn('typo %r -> %r' % (x, typos[x]))
+		oldcall(self, *k, **kw)
+	setattr(Build.BuildContext, m, call)
+
 def options(opt):
-	pass
+	"""
+	Add a few methods
+	"""
+
+	for m in meths_typos:
+		replace(m)
 
 def configure(conf):
 	pass
