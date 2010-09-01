a0a583a451c4e992ea21181864d73e59d63b147c
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-09-01 11:13:19 +0000

    docs

--- demos/swig/extend/java/Foo.java
+++ demos/swig/extend/java/Foo.java
@@ -5,4 +5,9 @@ class Foo {
 	public Foo() {
 		// TODO how to test the library from java?
 	}
+
+	public static void main(String[] args) {
+		A test = new A();
+		System.out.println(test.add(17, 28));
+	}
 }
