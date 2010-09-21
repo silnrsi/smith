9abed6f05f09927a8e372024f22462bea690bf9c
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-09-21 14:27:13 +0000

    set INCLUDES_CUDA, LIBPATH_CUDA and LIB_CUDA

--- playground/cuda/cuda.py
+++ playground/cuda/cuda.py
@@ -4,6 +4,7 @@
 
 "cuda"
 
+import os
 from waflib import Task
 from waflib.TaskGen import extension
 from waflib.Tools import ccroot, c_preproc
@@ -32,8 +33,27 @@ def find_cuda_dirs(self):
 
 	use ctx.program(source='main.c', target='app', use='CUDA')
 	"""
+
+	if not self.env.NVCC:
+		self.fatal('check for nvcc first')
+
+	d = self.root.find_node(self.env.NVCC).parent.parent
+
+	node = d.find_node('include')
+	_includes = node and node.abspath() or ''
+
+	node = d.find_node('lib')
+	_libpath = node and node.abspath() or ''
+
+	# this should not raise any error
+	self.check_cxx(header='cuda.h', lib='cuda', libpath=_libpath, includes=_includes)
+
 	# TODO set conf.env.LIB_CUDA = ['cuda']
 	# TODO set conf.env.INCLUDES_CUDA = ["path1", "path2"]
 	# TODO set conf.env.LIBPATH_CUDA = ["libpath1"]
-	pass
+
+	#self.env.LIB_CUDA = ['cuda', 'cudart']
+	#self.env.LIBPATH_CUDA = ['/comp/cuda/lib64']
+	#self.env.RPATH_CUDA   = ['/comp/cuda/lib64']
+
 
