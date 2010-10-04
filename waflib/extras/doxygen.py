e030ff291436e2c0feb3bbf735fdc957b2632c34
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-04 12:47:40 +0000

    at least it does something now (still not ready)

--- waflib/extras/doxygen.py
+++ waflib/extras/doxygen.py
@@ -8,7 +8,7 @@ ported from waf 1.5 (incomplete)
 
 from fnmatch import fnmatchcase
 import os, os.path, re, stat
-from waflib import Task, Utils, Node, Constants
+from waflib import Task, Utils, Node
 from waflib.TaskGen import feature
 
 DOXY_STR = '${DOXYGEN} - '
@@ -43,71 +43,12 @@ def read_into_dict(name):
 		ret[tmp[0].strip()] = '='.join(tmp[1:]).strip()
 	return ret
 
-def update_build_dir(node, env):
-	"""call this method when the contents of the build dir has to be read
-	(remove or create nodes, set the cache for ant_glob, ...)
-	make certain to avoid concurrent access (execute from the main thread)"""
-	path = node.abspath(env)
-
-	lst = Utils.listdir(path)
-	try:
-		node.__class__.bld.cache_dir_contents[node.id].update(lst)
-	except KeyError:
-		node.__class__.bld.cache_dir_contents[node.id] = set(lst)
-
-	ex = node.childs.keys()
-	for k in ex:
-		if node.childs[k].id & 3 != Node.FILE:
-			if not k in lst:
-				del node.childs[k]
-
-	for k in lst:
-		npath = path + os.sep + k
-		st = os.stat(npath)
-		if stat.S_ISREG(st[stat.ST_MODE]):
-			node.exclusive_build_node(k)
-		elif stat.S_ISDIR(st[stat.ST_MODE]):
-			parent = node.ensure_dir_node_from_path(k)
-			update_build_dir(parent, env)
-
-def nodes_files_of(node, recurse, includes, excludes):
-	"""
-	Recursively returns subnodes of this node. All node filenames that
-	matches a pattern in includes but does not match a pattern in
-	excludes is returned.
-
-	@param includes: list of wildcards to include.
-	@param excludes: list of wildcards to exclude.
-	@param recurse: whether to recurse or not.
-	"""
-	# perform the listdir in the source directory, once
-	node.__class__.bld.rescan(node)
-
-	# doxygen looks at the files under the source directory
-	buf = []
-	for x in node.__class__.bld.cache_dir_contents[node.id]:
-		filename = node.abspath() + os.sep + x
-		if any(fnmatchcase(filename, pat) for pat in excludes):
-			continue
-		st = os.stat(filename)
-		if stat.S_ISREG(st[stat.ST_MODE]):
-			k = node.find_resource(x)
-			if not any(fnmatchcase(k.abspath(), pat) for pat in includes):
-				continue
-			buf.append(k)
-		elif stat.S_ISDIR(st[stat.ST_MODE]) and recurse:
-			nd = node.find_dir(x)
-			if nd.id != nd.__class__.bld.bldnode.id:
-				buf += nodes_files_of(nd, recurse, includes, excludes)
-	return buf
-
-
 class doxygen_task(Task.Task):
-
-	vars = ['DOXYGEN', 'DOXYFLAGS']
-	color = 'BLUE'
-	after = 'cxx_link cc_link'
-	quiet = True
+	vars  = ['DOXYGEN', 'DOXYFLAGS']
+	color  = 'BLUE'
+	after  = 'cxx_link cc_link'
+	before = ['tar']
+	quiet  = True
 
 	def runnable_status(self):
 		'''
@@ -115,10 +56,10 @@ class doxygen_task(Task.Task):
 		run *before* both self.pars "consumers" - scan() and run()
 		'''
 		if not getattr(self, 'pars', None):
-			infile = self.inputs[0].abspath(self.env)
+			infile = self.inputs[0].abspath()
 			self.pars = read_into_dict(infile)
 			if not self.pars.get('OUTPUT_DIRECTORY'):
-				self.pars['OUTPUT_DIRECTORY'] = self.inputs[0].parent.abspath(self.env)
+				self.pars['OUTPUT_DIRECTORY'] = self.inputs[0].parent.abspath()
 			if not self.pars.get('INPUT'):
 				self.pars['INPUT'] = self.inputs[0].parent.abspath()
 		self.signature()
@@ -131,8 +72,8 @@ class doxygen_task(Task.Task):
 		if not includes:
 			includes = DOXY_EXTS
 
-		root_node = self.inputs[0].parent
-		ret = nodes_files_of(root_node, recurse, includes, excludes)
+		# TODO
+		ret = self.inputs[0].parent.ant_glob('**/*.cpp')
 		return (ret, [])
 
 	def run(self):
@@ -146,25 +87,19 @@ class doxygen_task(Task.Task):
 		return proc.returncode
 
 	def post_run(self):
-		# look for the files that appeared in the build directory
-		input_parent = self.inputs[0].parent
-		update_build_dir(input_parent, self.env)
-		lst = Utils.listdir(input_parent.abspath(self.env))
-		for k in DOXY_FMTS:
-			key = 'GENERATE_' + k.upper()
-			if self.pars.get(key) == 'YES':
-				if k in lst:
-					nd = input_parent.find_dir(k)
-					self.outputs += nd.find_iter(src=0, bld=1, dir=0)
 
-		self.outputs = [x for x in self.outputs if x.id & 3 != Node.DIR]
+		nodes = self.inputs[0].parent.get_bld().ant_glob('**/*')
+		for x in nodes:
+			x.sig = Utils.h_file(x.abspath())
+		self.outputs += nodes
+
 		return Task.Task.post_run(self)
 
-	def install(self):
-		if getattr(self.generator, 'install_to', None):
-			update_build_dir(self.inputs[0].parent, self.env)
-			pattern = getattr(self, 'instype', 'html/*')
-			self.generator.bld.install_files(self.generator.install_to, self.generator.path.ant_glob(pattern, dir=0, src=0))
+	#def install(self):
+	#	if getattr(self.generator, 'install_to', None):
+	#		update_build_dir(self.inputs[0].parent, self.env)
+	#		pattern = getattr(self, 'instype', 'html/*')
+	#		self.generator.bld.install_files(self.generator.install_to, self.generator.path.ant_glob(pattern, dir=0, src=0))
 
 class tar(Task.Task):
 	"quick tar creation"
@@ -173,7 +108,7 @@ class tar(Task.Task):
 	def runnable_status(self):
 		for x in getattr(self, 'input_tasks', []):
 			if not x.hasrun:
-				return Constants.ASK_LATER
+				return Task.ASK_LATER
 
 		if not getattr(self, 'tar_done_adding', None):
 			# execute this only once
@@ -181,7 +116,7 @@ class tar(Task.Task):
 			for x in getattr(self, 'input_tasks', []):
 				self.set_inputs(x.outputs)
 			if not self.inputs:
-				return Constants.SKIP_ME
+				return Task.SKIP_ME
 		return Task.Task.runnable_status(self)
 
 	def __str__(self):
@@ -194,22 +129,22 @@ def process_doxy(self):
 		return
 
 	node = self.path.find_resource(self.doxyfile)
-	if not node: raise ValueError, 'doxygen file not found'
+	if not node:
+		raise ValueError('doxygen file not found')
 
 	# the task instance
-	dsk = self.create_task('doxygen')
-	dsk.set_inputs(node)
+	dsk = self.create_task('doxygen', node)
 
 	if getattr(self, 'doxy_tar', None):
 		tsk = self.create_task('tar')
 		tsk.input_tasks = [dsk]
 		tsk.set_outputs(self.path.find_or_declare(self.doxy_tar))
 		if self.doxy_tar.endswith('bz2'):
-			tsk.env['TAROPTS'] = ' cjf '
+			tsk.env['TAROPTS'] = ['cjf']
 		elif self.doxy_tar.endswith('gz'):
-			tsk.env['TAROPTS'] = ' czf '
+			tsk.env['TAROPTS'] = ['czf']
 		else:
-			tsk.env['TAROPTS'] = ' cf '
+			tsk.env['TAROPTS'] = ['cf']
 
 def configure(conf):
 	conf.find_program('doxygen', var='DOXYGEN')
