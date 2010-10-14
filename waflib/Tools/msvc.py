73a5714804d755ac816b175ec415913679400040
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-14 11:45:38 +0000

    another flag missing

--- waflib/Tools/msvc.py
+++ waflib/Tools/msvc.py
@@ -577,7 +577,9 @@ def msvc_common_flags(conf):
 	v['STLIB_ST']     = 'lib%s.lib' # Note: to be able to distinguish between a static lib and a dll import lib, it's a good pratice to name the static lib 'lib%s.lib' and the dll import lib '%s.lib'
 	v['STLIBPATH_ST'] = '/LIBPATH:%s'
 
-	v['LINKFLAGS']            = ['/NOLOGO', '/MANIFEST']
+	v['LINKFLAGS']            = ['/NOLOGO']
+	if v['MSVC_MANIFEST']:
+		v.append_value('LINKFLAGS', '/MANIFEST')
 
 	# shared library
 	v['CCFLAGS_cshlib']  = ['']
