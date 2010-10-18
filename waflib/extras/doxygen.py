cc79fc317dc1ef23555d776136d34af54ab7edb3
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-18 11:44:54 +0000

    python3 fix

--- waflib/extras/doxygen.py
+++ waflib/extras/doxygen.py
@@ -50,6 +50,7 @@ class doxygen(Task.Task):
 
 	def run(self):
 		code = '\n'.join(['%s = %s' % (x, self.pars[x]) for x in self.pars])
+		code = code.encode() # for python 3
 		#fmt = DOXY_STR % (self.inputs[0].parent.abspath())
 		cmd = Utils.subst_vars(DOXY_STR, self.env)
 		proc = Utils.subprocess.Popen(cmd, shell=True, stdin=Utils.subprocess.PIPE)
