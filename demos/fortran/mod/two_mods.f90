b58918914ffe60ea89597e646d5014cf0b6cac13
kwmsmith <kwmsmith@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-20 23:15:20 +0000

    added test case for fortran module dependency

--- demos/fortran/mod/two_mods.f90
+++ demos/fortran/mod/two_mods.f90
@@ -1,11 +1,16 @@
-
-
 module mod1
 end module mod1
 
  module mod2
  use mod1
     integer :: mod2_int
+
+    ! FIXME:
+    ! replace the following line with the commented version, and recompile.
+    ! mod/uses_two_mods.f90 should be recompiled, but currently isn't.
+
+    integer, parameter :: mod2_param = 5
+    ! integer, parameter :: mod2_param = 6
 
     interface mod2_proc
         module procedure mod2_proc1, mod2_proc2
