396313212e1694a643c1dd68b3886e3faa87f6a6
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-26 21:17:10 +0000

    We will release a new version for #773

--- waflib/Context.py
+++ waflib/Context.py
@@ -11,13 +11,13 @@ from waflib import Utils, Errors, Logs
 import waflib.Node
 
 # the following 3 constants are updated on each new release (do not touch)
-HEXVERSION=0x1060000
+HEXVERSION=0x1060100
 """constant updated on new releases"""
 
-WAFVERSION="1.6.0"
+WAFVERSION="1.6.1"
 """constant updated on new releases"""
 
-WAFREVISION="10085"
+WAFREVISION="10135"
 """constant updated on new releases"""
 
 ABI = 98
