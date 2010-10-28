3389db3a8d8542d34b25663a675ad74068ea3637
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-10-28 22:01:41 +0000

    #774

--- waflib/extras/cs.py
+++ waflib/extras/cs.py
@@ -9,11 +9,13 @@ We will need a demo to check that this works
 bld(features='cs', source='main.cs', gen='foo')
 """
 
-from waflib import TaskGen, Utils, Task, Options, Logs
+from waflib import TaskGen, Utils, Task, Options, Logs, Errors
 from TaskGen import before, after, feature
 from waflib.Tools import ccroot
+from waflib.Configure import conf
 
 ccroot.USELIB_VARS['cs'] = set(['CSFLAGS', 'ASSEMBLIES', 'RESOURCES'])
+ccroot.lib_patterns['csshlib'] = ['%s']
 
 @feature('cs')
 @after('apply_uselib_cs')
@@ -38,12 +40,19 @@ def use_cs(self):
 	names = self.to_list(getattr(self, 'use', []))
 	get = self.bld.get_tgen_by_name
 	for x in names:
-		y = get(x)
+		try:
+			y = get(x)
+		except Errors.WafError:
+			self.cs_task.env.append_value('CSFLAGS', '/reference:%s' % x)
+			continue
 		y.post()
-		self.cs_task.set_run_after(y.cs_task) # order
-		self.cs_task.dep_nodes.extend(y.cs_task.outputs) # dependency
 
-		self.cs_task.env.append_value('CSFLAGS', '/reference:%s' % y.cs_task.outputs[0].abspath())
+		tsk = getattr(y, 'cs_task', None) or getattr(y, 'link_task', None)
+		if not tsk:
+			self.bld.fatal('cs task has no link task for use %r' % self)
+		self.cs_task.set_run_after(tsk) # order
+		self.cs_task.dep_nodes.extend(tsk.outputs) # dependency
+		self.cs_task.env.append_value('CSFLAGS', '/reference:%s' % tsk.outputs[0].abspath())
 
 class mcs(Task.Task):
 	color   = 'YELLOW'
@@ -59,4 +68,19 @@ def configure(conf):
 
 def options(opt):
 	opt.add_option('--with-csc-binary', type='string', dest='cscbinary')
+
+class fake_csshlib(Task.Task):
+	"""task used for reading a foreign .net assembly and adding the dependency on it"""
+	color   = 'YELLOW'
+	inst_to = None
+
+	def runnable_status(self):
+		for x in self.outputs:
+			x.sig = Utils.h_file(x.abspath())
+		return Task.SKIP_ME
+
+@conf
+def read_csshlib(self, name, paths=[]):
+	"""read a foreign .net assembly for the use system"""
+	return self(name=name, features='fake_lib', lib_paths=paths, lib_type='csshlib')
 
