7ce0e668dbe5b86bfd0cd44a908e28edc93e3529
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-11-11 14:26:49 +0000

    docs

--- waflib/ConfigSet.py
+++ waflib/ConfigSet.py
@@ -15,26 +15,34 @@ re_imp = re.compile('^(#)*?([^#=]*?)\ =\ (.*?)$', re.M)
 
 class ConfigSet(object):
 	"""
-	A dict that honor serialization and parent relationships
+	A dict that honor serialization and parent relationships. The serialization format
+	is human-readable (python-like) and performed by using eval() and repr().
+	For high performance prefer pickle. Do not store functions as they are not serializable.
 
-	Store and retrieve values easily and in a human-readable format
+	The values can be accessed by attributes or by keys::
 
-	it is not possible to serialize functions though
-
+		from waflib.ConfigSet import ConfigSet
+		env = ConfigSet()
+		env.FOO = 'test'
+		env['FOO'] = 'test'
 	"""
 	__slots__ = ('table', 'parent')
 	def __init__(self, filename=None):
-		"""
-		The internal dict is kept in self.table
-		"""
 		self.table = {}
+		"""
+		Internal dict holding the object values
+		"""
 		#self.parent = None
 
 		if filename:
 			self.load(filename)
 
 	def __contains__(self, key):
-		"""required to use foo in env"""
+		"""Required to use::
+
+			if 'foo' in env:
+				print env['foo']
+		"""
 		if key in self.table: return True
 		try: return self.parent.__contains__(key)
 		except AttributeError: return False # parent may not exist
@@ -120,7 +128,16 @@ class ConfigSet(object):
 
 	def derive(self):
 		"""
-		Returns a new ConfigSet deriving from self
+		Returns a new ConfigSet deriving from self. The copy returned
+		will be a shallow copy::
+
+			from waflib.ConfigSet import ConfigSet
+			env = ConfigSet()
+			env.append_value('CFLAGS', ['-O2'])
+			child = env.derive()
+			child.CFLAGS.append('test') # warning! this will modify 'env'
+			child.CFLAGS = ['-O3'] # new list, ok
+			child.append_value('CFLAGS', ['-O3']) # ok
 
 		Use :py:func:`ConfigSet.detach` to detach the child from the parent.
 		"""
@@ -132,7 +149,7 @@ class ConfigSet(object):
 		"""
 		Detach self from its parent (if existing)
 
-		Modifying the parent :py:class:`ConfigSet` will not change this one.
+		Modifying the parent :py:class:`ConfigSet` will not change the current object
 		Modifying this :py:class:`ConfigSet` will not modify the parent one.
 		"""
 		tbl = self.get_merged_dict()
@@ -148,20 +165,22 @@ class ConfigSet(object):
 
 	def get_flat(self, key):
 		"""
-		Obtain a value as a string
+		Return a value as a string. If the input is a list, the value returned is space-separated.
+
+		:param key: key to use
+		:type key: string
 		"""
 		s = self[key]
 		if isinstance(s, str): return s
 		return ' '.join(s)
 
 	def _get_list_value_for_modification(self, key):
-		"""Gets a value that must be a list for further modification.
-		
+		"""
+		Return a list value for further modification.
+
 		The	list may be modified inplace and there is no need to do::
 			self.table[var] = value
 		afterwards.
-
-		This is private btw
 		"""
 		try:
 			value = self.table[key]
@@ -180,7 +199,10 @@ class ConfigSet(object):
 
 	def append_value(self, var, val):
 		"""
-		Appends a value to the specified config key
+		Appends a value to the specified config key::
+
+			def build(bld):
+				bld.env.append_unique('CFLAGS', ['-O2'])
 
 		The value must be a list or a tuple
 		"""
@@ -191,7 +213,10 @@ class ConfigSet(object):
 
 	def prepend_value(self, var, val):
 		"""
-		Prepends a value to the specified item
+		Prepends a value to the specified item::
+
+			def configure(conf):
+				conf.env.prepend_value('CFLAGS', ['-O2'])
 
 		The value must be a list or a tuple
 		"""
@@ -201,9 +226,10 @@ class ConfigSet(object):
 
 	def append_unique(self, var, val):
 		"""
-		Appends a value to the specified item only if it's not already present
+		Append a value to the specified item only if it's not already present::
 
-		The value must be a list or a tuple
+			def build(bld):
+				bld.env.append_value('CFLAGS', ['-O2', '-g'])
 
 		Note that there is no prepend_unique
 		"""
@@ -217,7 +243,9 @@ class ConfigSet(object):
 
 	def get_merged_dict(self):
 		"""
-		Computes the merged dictionary from the fusion of self and all its parent
+		Compute the merged dictionary from the fusion of self and all its parent
+
+		:rtype: a ConfigSet object
 		"""
 		table_list = []
 		env = self
@@ -231,7 +259,12 @@ class ConfigSet(object):
 		return merged_table
 
 	def store(self, filename):
-		"Writes the :py:class:`ConfigSet` data into a file"
+		"""
+		Write the :py:class:`ConfigSet` data into a file. See :py:meth:`ConfigSet.load` for reading such files.
+
+		:param filename: file to use
+		:type filename: string
+		"""
 		f = None
 		try:
 			f = open(filename, 'w')
@@ -246,7 +279,12 @@ class ConfigSet(object):
 				f.close()
 
 	def load(self, filename):
-		"Retrieve the :py:class:`ConfigSet` data from a file"
+		"""
+		Retrieve the :py:class:`ConfigSet` data from a file. See :py:meth:`ConfigSet.store` for writing such files
+
+		:param filename: file to use
+		:type filename: string
+		"""
 		tbl = self.table
 		code = Utils.readf(filename)
 		for m in re_imp.finditer(code):
@@ -257,22 +295,33 @@ class ConfigSet(object):
 	def update(self, d):
 		"""
 		Dictionary interface: replace values from another dict
+
+		:param d: object to use the value from
+		:type d: dict-like object
 		"""
 		for k, v in d.items():
 			self[k] = v
 
 	def stash(self):
 		"""
-		Stores the object state, to use with 'revert' below
+		Store the object state, to provide a kind of transaction support::
 
-		Used to have some kind of transaction support.
+			env = ConfigSet()
+			env.stash()
+			try:
+				env.append_value('CFLAGS', '-O3')
+				call_some_method(env)
+			finally:
+				env.revert()
+
+		The history is kept in a stack, and is lost during the serialization by :py:meth:`ConfigSet.store`
 		"""
 		self.undo_stack = self.undo_stack + [self.table]
 		self.table = self.table.copy()
 
 	def revert(self):
 		"""
-		Reverts the object to a previous state, to use with 'stash' above
+		Reverts the object to a previous state. See :py:meth:`ConfigSet.stash`
 		"""
 		self.table = self.undo_stack.pop(-1)
 
