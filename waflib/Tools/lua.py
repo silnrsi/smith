667fb98e14485123d74b58081364eef24869e727
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-11-19 20:44:58 +0000

    docs

--- waflib/Tools/lua.py
+++ waflib/Tools/lua.py
@@ -3,6 +3,17 @@
 # Sebastian Schlingmann, 2008
 # Thomas Nagy, 2008-2010 (ita)
 
+"""
+Lua support.
+
+Compile *.lua* files into *.luac*::
+
+	def configure(conf):
+		conf.load('lua')
+	def build(bld):
+		bld(source='foo.lua')
+"""
+
 from waflib import TaskGen, Utils
 
 TaskGen.declare_chain(
@@ -12,12 +23,10 @@ TaskGen.declare_chain(
 	ext_out = '.luac',
 	reentrant = False
 )
-	#install = 'LUADIR', # env variable TODO
-
-@TaskGen.feature('lua')
-def init_lua(self):
-	self.default_chmod = Utils.O755
 
 def configure(conf):
+	"""
+	Detect the luac compiler and set *conf.env.LUAC*
+	"""
 	conf.find_program('luac', var='LUAC')
 
