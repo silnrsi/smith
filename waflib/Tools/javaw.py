18f558d2514c9d81d41556f75c568b3c4d59e0ff
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-12-05 20:25:36 +0000

    better regexp

--- waflib/Tools/javaw.py
+++ waflib/Tools/javaw.py
@@ -38,8 +38,8 @@ ccroot.USELIB_VARS['javac'] = set(['CLASSPATH', 'JAVACFLAGS'])
 
 SOURCE_RE = '**/*.java'
 JAR_RE = '**/*'
-re_verbose = re.compile(r'\[.*?\]$', re.M)
-re_classes = re.compile(r'\[wrote\ (.*?\.class)\]')
+re_verbose = re.compile(r'^\[.*?\]\n*', re.M)
+re_classes = re.compile(r'\[wrote (.*?\.class)\]')
 
 class_check_source = '''
 public class Test {
