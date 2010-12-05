f5958d3406c6d9eb94d662671ba299be4160cfcb
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-12-05 17:46:34 +0000

    docs

--- playground/custom_build_system/bbdlib.py
+++ playground/custom_build_system/bbdlib.py
@@ -1,5 +1,37 @@
 #! /usr/bin/env python
 
-def start(current_directory, version, wafdir):
-	print(current_directory, version, wafdir)
+import os, sys, imp
+from waflib import Context, Options, Configure, Utils
 
+def start(cwd, version, wafdir):
+	try:
+		os.stat(cwd + '/bbit')
+	except:
+		print('call from a folder containing a file named "bbit"')
+		sys.exit(1)
+
+	Context.waf_dir = wafdir
+	Context.top = Context.run_dir = cwd
+	Context.out = os.path.join(cwd, 'build')
+	Context.g_module = imp.new_module('wscript')
+	Context.g_module.root_path = os.path.join(cwd, 'bbit')
+	Context.Context.recurse = \
+		lambda x, y: getattr(Context.g_module, x.cmd, Utils.nada)(x)
+
+	Context.g_module.configure = lambda ctx: ctx.load('g++')
+	Context.g_module.build = lambda bld: bld.objects(source='main.c')
+
+	opt = Options.OptionsContext().execute()
+
+	do_config = 'configure' in sys.argv
+	try:
+		os.stat(cwd + '/build')
+	except:
+		do_config = True
+	if do_config:
+		Context.create_context('configure').execute()
+
+	if 'clean' in sys.argv:
+		Context.create_context('clean').execute()
+	if 'build' in sys.argv:
+		Context.create_context('build').execute()
