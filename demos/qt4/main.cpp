e85f7f52c966e7a4423263e73f970a099f43b566
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2010-12-30 21:57:25 +0000

    new example

---
+++ demos/qt4/main.cpp
@@ -0,0 +1,13 @@
+
+#include <QApplication>
+
+#include "ui_but.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+	QWidget window;
+    Ui::Form ui;
+    ui.setupUi(&window);
+    return app.exec();
+}
