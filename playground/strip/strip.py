9ea2d171f47168232db3a83a371f4d018aa0448b
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-01-11 22:24:49 +0000

    docs

--- playground/strip/strip.py
+++ playground/strip/strip.py
@@ -1,5 +1,18 @@
 #! /usr/bin/env python
 
+"""
+Strip a program/library after it is created. Use this tool as an example.
+
+Usage::
+
+	bld.program(features='strip', source='main.c', target='foo')
+
+By using::
+
+	@TaskGen.feature('cprogram', 'cxxprogram', 'fcprogram')
+
+It is possible to have all the C, C++ and Fortran programs stripped automatically
+"""
 
 def configure(conf):
 	conf.find_program('strip')
