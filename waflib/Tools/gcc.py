148dcc48e96f0bd214b5387ddfe0b44b4f9ba6cc
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-01-18 12:44:19 +0000

    hpux fixes

--- waflib/Tools/gcc.py
+++ waflib/Tools/gcc.py
@@ -124,7 +124,10 @@ def gcc_modifier_aix(conf):
 
 @conf
 def gcc_modifier_hpux(conf):
-	conf.env.cshlib_PATTERN = 'lib%s.sl'
+	v=conf.env
+	v['SHLIB_MARKER']        = []
+	v['CFLAGS_cshlib']       = ['-fPIC','-DPIC']
+	v['cshlib_PATTERN']      = 'lib%s.sl'
 
 @conf
 def gcc_modifier_platform(conf):
