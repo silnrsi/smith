38f5c984e02f2c6f5a4541ffd6820d94b55184ca
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-03-02 00:20:43 +0000

    docs

---
+++ playground/exclusive_link/excl.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Thomas Nagy, 2011 (ita)
+
+from waflib.Utils import threading
+from waflib import Task
+lock = threading.Lock()
+
+def make_exclusive(cls):
+	old = cls.runnable_status
+	def runnable_status(self):
+		ret = Task.ASK_LATER
+		if lock.acquire(blocking=False):
+			try:
+				ret = old(self)
+			finally:
+				lock.release()
+		return ret
+
+for x in 'cprogram cxxprogram cshlib cxxshlib cstlib cxxstlib'.split():
+	if x in Task.classes:
+		make_exclusive(Task.classes[x])
+
