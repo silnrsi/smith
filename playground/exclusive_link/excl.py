bb818f0df3dff29b82004d4485bda5cb0e7b1dce
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-03-02 11:18:16 +0000

    what did i have in mind when i wrote this

--- playground/exclusive_link/excl.py
+++ playground/exclusive_link/excl.py
@@ -11,16 +11,16 @@ from waflib import Task
 lock = threading.Lock()
 
 def make_exclusive(cls):
-	old = cls.runnable_status
-	def runnable_status(self):
-		ret = Task.ASK_LATER
-		if lock.acquire(False):
-			try:
-				ret = old(self)
-			finally:
-				lock.release()
+	old_run = cls.run
+	def run(self):
+		ret = None
+		lock.acquire()
+		try:
+			ret = old_run(self)
+		finally:
+			lock.release()
 		return ret
-	cls.runnable_status = runnable_status
+	cls.run = run
 
 for x in 'cprogram cxxprogram cshlib cxxshlib cstlib cxxstlib'.split():
 	if x in Task.classes:
