56f6fd18bd7f54f3f10941be3a9457e969234002
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-03-08 20:04:15 +0000

    remove the 'r' in revision

--- waflib/Context.py
+++ waflib/Context.py
@@ -17,7 +17,7 @@ HEXVERSION=0x1060300
 WAFVERSION="1.6.3"
 """Constant updated on new releases"""
 
-WAFREVISION="r$Revision$"
+WAFREVISION="$Revision$"
 """Constant updated on new releases"""
 
 ABI = 98
