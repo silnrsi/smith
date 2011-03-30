de031a8ae39cf2e36a53659ac5239f868aac5d3f
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-03-30 00:11:12 +0000

    #854

--- waflib/Tools/flex.py
+++ waflib/Tools/flex.py
@@ -15,13 +15,26 @@ def decide_ext(self, node):
 		return ['.lex.cc']
 	return ['.lex.c']
 
+def flexfun(tsk):
+	env = tsk.env
+	bld = tsk.generator.bld
+	wd = bld.variant_dir
+	def to_list(xx):
+		if isinstance(xx, str): return [xx]
+		return xx
+	tsk.last_cmd = lst = []
+	lst.extend(to_list(env['FLEX']))
+	lst.extend(to_list(env['FLEXFLAGS']))
+	lst.extend([a.path_from(bld.bldnode) for a in tsk.inputs])
+	lst = [x for x in lst if x]
+	txt = bld.cmd_and_log(lst, cwd=wd, env=env.env or None, quiet=0)
+	tsk.outputs[0].write(txt)
+
 waflib.TaskGen.declare_chain(
 	name = 'flex',
-	#rule = '${FLEX} -t ${FLEXFLAGS} ${SRC} > ${TGT}',
-	rule = '${FLEX} -o${TGT} ${FLEXFLAGS} ${SRC}', #854
+	rule = flexfun, # issue #854
 	ext_in = '.l',
 	decider = decide_ext,
-	shell = True,
 )
 
 def configure(conf):
@@ -29,4 +42,5 @@ def configure(conf):
 	Detect the *flex* program
 	"""
 	conf.find_program('flex', var='FLEX')
+	conf.env.FLEXFLAGS = ['-t']
 
