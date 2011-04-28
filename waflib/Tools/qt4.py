7de8de4e903b7f97af8a11b9c1ed6cdbb0dd9dea
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-04-28 00:09:31 +0000

    add the .ui files for translation

--- waflib/Tools/qt4.py
+++ waflib/Tools/qt4.py
@@ -274,7 +274,8 @@ def apply_qt4(self):
 			qmtasks.append(self.create_task('ts2qm', x, x.change_ext('.qm')))
 
 		if getattr(self, 'update', None) and Options.options.trans_qt4:
-			cxxnodes = [a.inputs[0] for a in self.compiled_tasks]
+			cxxnodes = [a.inputs[0] for a in self.compiled_tasks] + [
+                 a.inputs[0] for a in self.tasks if getattr(a, 'inputs', None) and a.inputs[0].name.endswith('.ui')]
 			for x in qmtasks:
 				self.create_task('trans_update', cxxnodes, x.inputs)
 
