260699992cf6bc1e7a43f2e304124bce73cf5379
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-05-02 22:17:22 +0000

    more code

--- playground/lecache.py/lecache.py
+++ playground/lecache.py/lecache.py
@@ -3,26 +3,46 @@
 import os
 import SocketServer
 
-SIZE = 100
+CONN = ('', 51200)
+SIZE = 50
 BUF = 8192
+
+tmp = '/home/waf/plat.jpg'
+GET = 2
+PUT = 3
 
 class req(SocketServer.StreamRequestHandler):
 	def handle(self):
-		self.rfile.read(SIZE)
+		query = self.rfile.read(SIZE).strip().split(',')
+		if len(query) == GET:
+			# get a file from the cache if it exists, else return 0
+			tmp = os.path.join(query[0], query[1])
+			fsize = 0
+			try:
+				fsize = os.stat(tmp).st_size
+			except:
+				pass
+			params = [str(fsize)]
+			self.wfile.write(','.join(params).ljust(SIZE))
 
-		p = '/home/waf/plat.jpg'
-		fsize = os.stat(p).st_size
-		params = [str(fsize)]
-		self.wfile.write(','.join(params).ljust(SIZE))
-		f = open(p, 'rb')
-		cnt = 0
-		while cnt < fsize:
-			r = f.read(BUF)
-			self.wfile.write(r)
-			cnt += len(r)
-		print "transfer complete"
+			if not fsize:
+				print "file not found in cache"
+				return
 
-server = SocketServer.TCPServer( ('', 51200), req)
+			f = open(tmp, 'rb')
+			cnt = 0
+			while cnt < fsize:
+				r = f.read(BUF)
+				self.wfile.write(r)
+				cnt += len(r)
+			print "get complete"
+		elif len(query) == PUT:
+			# add a file to the cache, the tird parameter is the file size
+			pass
+		else:
+			print "invalid query", query
+
+server = SocketServer.TCPServer(CONN, req)
 server.serve_forever()
 
 
