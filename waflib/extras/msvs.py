aebb3686af0587aa9c4eb0c0d2f5f3c849838bab
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-05-15 10:23:45 +0000

    even more code

--- waflib/extras/msvs.py
+++ waflib/extras/msvs.py
@@ -36,6 +36,7 @@ import uuid # requires python 2.5
 from waflib.Build import BuildContext
 from waflib import Utils, TaskGen, Logs
 
+
 BIN_GUID_PREFIX = Utils.md5('BIN').hexdigest()[:8].upper()
 LIB_GUID_PREFIX = Utils.md5('LIB').hexdigest()[:8].upper()
 SPU_GUID_PREFIX = Utils.md5('SPU').hexdigest()[:8].upper()
@@ -48,6 +49,104 @@ VS_GUID_VCPROJ         = "8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942"
 VS_GUID_SOLUTIONFOLDER = "2150E333-8FDC-42A3-9474-1A3956D46DE8"
 
 HEADERS_GLOB = '**/*.h|*.hpp|*.H|*.inl'
+
+PROJECT_TEMPLATE = '''<?xml version="1.0" encoding="Windows-1252"?>
+<Project
+	DefaultTargets="Build"
+	ToolsVersion="4.0"
+	xmlns="http://schemas.microsoft.com/developer/msbuild/2003"
+	>
+
+	<ItemGroup Label="ProjectConfigurations">
+%(projectconfigurations)s
+	</ItemGroup>
+
+	<PropertyGroup Label="Globals">
+		<ProjectGuid>{%(guid)s}</ProjectGuid>
+		<Keyword>MakeFileProj</Keyword>
+	</PropertyGroup>
+	<Import Project="$(VCTargetsPath)\Microsoft.Cpp.Default.props" />
+
+%(propertygroups_pre)s
+
+	<Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />
+	<ImportGroup Label="ExtensionSettings">
+	</ImportGroup>
+
+%(propertysheets)s
+
+%(propertygroups_post)s
+
+%(itemdefinitions)s
+
+	<ItemGroup>
+%(sources)s
+	</ItemGroup>
+	<Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />
+	<ImportGroup Label="ExtensionTargets">
+	</ImportGroup>
+</Project>
+'''
+
+PROJECTCONFIGURATION_TEMPLATE = '''
+		<ProjectConfiguration Include="%(name)s|%(pname)s">
+			<Configuration>%(name)s</Configuration>
+			<Platform>%(pname)s</Platform>
+		</ProjectConfiguration>
+'''
+
+PROPERTYGROUP_PRE_TEMPLATE = '''
+	<PropertyGroup Condition="'$(Configuration)|$(Platform)'=='%(name)s|%(pname)s'" Label="Configuration">
+		<ConfigurationType>Makefile</ConfigurationType>
+		<OutDir>%(output_dir)s</OutDir>
+	</PropertyGroup>
+'''
+
+PROPERTYSHEET_TEMPLATE = '''
+	<ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='%(name)s|%(pname)s'">
+		<Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+	</ImportGroup>
+'''
+
+PROPERTYGROUP_POST_TEMPLATE = '''
+	<PropertyGroup Condition="'$(Configuration)|$(Platform)'=='%(name)s|%(pname)s'">
+		<NMakeBuildCommandLine>%(build_command)s</NMakeBuildCommandLine>
+		<NMakeReBuildCommandLine>%(rebuild_command)s</NMakeReBuildCommandLine>
+		<NMakeOutput>%(output_dir)s%(output_file)s</NMakeOutput>
+		<NMakeCleanCommandLine>%(clean_command)s</NMakeCleanCommandLine>
+		<NMakePreprocessorDefinitions>%(pre_defines)s;$(NMakePreprocessorDefinitions)</NMakePreprocessorDefinitions>
+	</PropertyGroup>
+'''
+
+PROPERTYGROUP_POST_TEMPLATE_XENON = '''
+	<PropertyGroup Condition="'$(Configuration)|$(Platform)'=='%(name)s|%(pname)s'">
+		<NMakeBuildCommandLine>%(build_command)s</NMakeBuildCommandLine>
+		<NMakeReBuildCommandLine>%(rebuild_command)s</NMakeReBuildCommandLine>
+		<NMakeOutput>%(output_dir)s%(output_file)s</NMakeOutput>
+		<NMakeCleanCommandLine>%(clean_command)s</NMakeCleanCommandLine>
+		<NMakePreprocessorDefinitions>%(pre_defines)s;$(NMakePreprocessorDefinitions)</NMakePreprocessorDefinitions>
+		<RemoteRoot>%(deploy_dir)s</RemoteRoot>
+	</PropertyGroup>
+'''
+
+ITEMDEFINITION_TEMPLATE_XENON = '''
+	<ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='%(name)s|%(pname)s'">
+		<Deploy>
+			<DeploymentType>CopyToHardDrive</DeploymentType>
+		</Deploy>
+	</ItemDefinitionGroup>
+'''
+
+FILTER_TEMPLATE = '''<?xml version="1.0" encoding="Windows-1252"?>
+<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+%s
+</Project>
+'''
+
+JOIN_SEPARATOR = { "configurations" : "\n",
+                   "include_search_path" : ";",
+                   "include_dirs" : "@",}
+
 
 class msvs_generator(BuildContext):
 	cmd = 'msvs'
@@ -184,7 +283,6 @@ class msvs_generator(BuildContext):
 				'flags_final'   : '',
 				'include_dirs'  : ''
 				}
-		# self-referencing hash
 		values['guid'] = self.make_guid(values, prefix = guid)
 
 		out = self.bldnode.make_node('depprojs')
@@ -192,18 +290,67 @@ class msvs_generator(BuildContext):
 		proj_file   = out.make_node('%s_%s.vcxproj' % (project, platform))
 		filter_file = out.make_node('%s_%s.vcxproj.filters' % (project, platform))
 
+		tg.post()
+		config = self.get_project_config(values, tg)
+		(proj_str, filter_str) = self.create_project_string(values, config)
+
 		Logs.warn('Creating %r' % proj_file)
-
-		#if guid == BIN_GUID_PREFIX:
-		#	(proj_str, filter_str) = createProjectString(values, getBinProjectConfigs(values))
-		#else:
-		#	(proj_str, filter_str) = createProjectString(values, getLibProjectConfigs(values))
-
-		proj_str = "oki"
-		filter_str = "oki"
-
 		proj_file.write(proj_str)
 		filter_file.write(filter_str)
 
 		return proj_file.abspath()
+
+	def get_project_name(self):
+		if self.platform == 'Xenon':
+			return 'Xbox 360'
+		return 'Win32'
+
+	def get_project_config(self, values, tg):
+		# getLibProjectConfigs(values)
+		link = getattr(tg, 'link_task', None)
+		return [{
+				'pname': self.get_project_name(),
+				'name': self.platform,
+				'build_command' : 'waf configure build',
+				'rebuild_command' : 'waf configure clean build',
+				'clean_command' : 'waf clean',
+				'output_dir' : link and link.outputs[0].parent.abspath() or '',
+				'output_file': link and link.outputs[0].abspath() or '',
+				'pre_defines': Utils.subst_vars('${DEFINES_ST:DEFINES}', tg.env)
+				}]
+
+	def create_project_string(self, values, configurations):
+		projectconfiguration_str	= ''
+		propertygroup_pre_str		= ''
+		propertysheet_str			= ''
+		propertygroup_post_str		= ''
+		itemdefinition_str			= ''
+
+		for cfg in configurations:
+			projectconfiguration_str += PROJECTCONFIGURATION_TEMPLATE % cfg
+			propertygroup_pre_str    += PROPERTYGROUP_PRE_TEMPLATE % cfg
+			propertysheet_str        += PROPERTYSHEET_TEMPLATE % cfg
+			propertygroup_post_str   += PROPERTYGROUP_POST_TEMPLATE % cfg
+			#if platform == 'Xenon':
+			#	propertygroup_post_str		+= propertygroup_post_templ_xenon % cc
+			#	if values['guid'][0:8] == BIN_GUID_PREFIX:
+			#		itemdefinition_str			+= itemdefinition_templ_xenon % cc
+			#else:
+			#	propertygroup_post_str		+= propertygroup_post_templ % cc
+
+		(file_str, filter_str) = self.gen_tree_string(values.get('sources', []), values['abs_path'])
+
+		project_str = PROJECT_TEMPLATE % { 'name'                  : values['name'],
+										   'guid'                  : values['guid'],
+										   'pname'                 : self.get_project_name(),
+										   'projectconfigurations' : projectconfiguration_str,
+										   'propertygroups_pre'    : propertygroup_pre_str,
+										   'propertysheets'        : propertysheet_str,
+										   'propertygroups_post'   : propertygroup_post_str,
+										   'itemdefinitions'       : itemdefinition_str,
+										   'sources'		       : file_str }
+		return (project_str, FILTER_TEMPLATE % filter_str)
+
+	def gen_tree_string(self, *k, **kw):
+		return ('', '')
 
