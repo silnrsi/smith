83da330084186c2d5882d6503ad939262716681d
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-06-11 19:22:10 +0000

    enable pipelining

--- playground/netcache/netcache_client.py
+++ playground/netcache/netcache_client.py
@@ -5,16 +5,35 @@
 import os, socket, asyncore, tempfile
 import Task, Constants
 
-BUF = 8192
-SIZE = 99
+BUF = 8192 * 16
+HEADER_SIZE = 128
 Task.net_cache = (socket.gethostname(), 51200)
 
-def recv_file(ssig, cnt, p):
+GET = 'GET'
+PUT = 'PUT'
+LST = 'LST'
+BYE = 'BYE'
+
+def get_connection():
+	# return a new connection... do not forget to close it!
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect(Task.net_cache)
-	params = (ssig, str(cnt))
-	s.send(','.join(params).ljust(SIZE))
-	data = s.recv(SIZE)
+
+def close_connection(conn):
+	if conn:
+		try:
+			conn.send(BYE.ljust(HEADER_SIZE))
+		except:
+			pass
+		try:
+			conn.close()
+		except:
+			pass
+
+def recv_file(conn, ssig, cnt, p):
+	params = (GET, ssig, str(cnt))
+	conn.send(','.join(params).ljust(HEADER_SIZE))
+	data = conn.recv(HEADER_SIZE)
 	size = int(data.split()[0])
 
 	if size == -1:
@@ -25,32 +44,28 @@ def recv_file(ssig, cnt, p):
 	f = open(p, 'wb')
 	cnt = 0
 	while cnt < size:
-		data = s.recv(min(BUF, size-cnt))
+		data = conn.recv(min(BUF, size-cnt))
 		if not data:
 			raise ValueError('connection ended %r %r' % (cnt, size))
 		f.write(data)
 		cnt += len(data)
 	f.close()
-	s.close()
 
-def put_data(ssig, cnt, p):
+def put_data(conn, ssig, cnt, p):
 	#print "pushing %r %r %r" % (ssig, cnt, p)
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(Task.net_cache)
 	size = os.stat(p).st_size
-	params = (ssig, str(cnt), str(size))
-	s.send(','.join(params).ljust(SIZE))
+	params = (PUT, ssig, str(cnt), str(size))
+	conn.send(','.join(params).ljust(HEADER_SIZE))
 	f = open(p, 'rb')
 	cnt = 0
 	while cnt < size:
 		r = f.read(min(BUF, size-cnt))
 		while r:
-			k = s.send(r)
+			k = conn.send(r)
 			if not k:
 				raise ValueError('connection ended')
 			cnt += k
 			r = r[k:]
-	s.close()
 
 def can_retrieve_cache(self):
 	if not Task.net_cache:
@@ -63,13 +78,16 @@ def can_retrieve_cache(self):
 	sig = self.signature()
 	ssig = self.unique_id().encode('hex') + sig.encode('hex')
 
+	conn = None
 	try:
+		conn = get_connection()
 		for node in self.outputs:
 			variant = node.variant(self.env)
 			p = node.abspath(self.env)
-			recv_file(ssig, cnt, p)
+			recv_file(conn, ssig, cnt, p)
 			cnt += 1
 	except Exception, e:
+		close_connection(conn)
 		return False
 
 	self.got_cached = True
@@ -82,29 +100,37 @@ def post_run(self):
 	sig = self.signature()
 	ssig = self.unique_id().encode('hex') + sig.encode('hex')
 
+	conn = None
+
 	cnt = 0
 	variant = env.variant()
-	for node in self.outputs:
-		try:
-			os.stat(node.abspath(env))
-		except OSError:
-			self.has_run = MISSING
-			self.err_msg = '-> missing file: %r' % node.abspath(env)
-			raise Utils.WafError
 
-		# important, store the signature for the next run
-		bld.node_sigs[variant][node.id] = sig
+	try:
+		for node in self.outputs:
+			try:
+				os.stat(node.abspath(env))
+			except OSError:
+				self.has_run = MISSING
+				self.err_msg = '-> missing file: %r' % node.abspath(env)
+				raise Utils.WafError
 
-		# We could re-create the signature of the task with the signature of the outputs
-		# in practice, this means hashing the output files
-		# this is unnecessary
-		try:
-			if Task.net_cache and not self.got_cached:
-				put_data(ssig, cnt, node.abspath(env))
-		except Exception, e:
-			#print "Could not restore the files", e
-			pass
-		cnt += 1
+			# important, store the signature for the next run
+			bld.node_sigs[variant][node.id] = sig
+
+			# We could re-create the signature of the task with the signature of the outputs
+			# in practice, this means hashing the output files
+			# this is unnecessary
+			try:
+				if Task.net_cache and not self.got_cached:
+					if not conn:
+						conn = get_connection()
+					put_data(conn, ssig, cnt, node.abspath(env))
+			except Exception, e:
+				#print "Could not restore the files", e
+				pass
+			cnt += 1
+	finally:
+		close_connection(conn)
 
 	bld.task_sigs[self.unique_id()] = self.cache_sig
 Task.Task.post_run = post_run
--- playground/netcache/netcache_server.py
+++ playground/netcache/netcache_server.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
-# Thomas Nagy 2011 (ita)
 # encoding: utf-8
+# Thomas Nagy 2011 (ita)
 
 """
 Simple TCP server to cache files over the network
@@ -16,13 +16,15 @@ import SocketServer
 
 CACHEDIR = '/tmp/wafcache'
 CONN = (socket.gethostname(), 51200)
-SIZE = 99
-BUF = 8192
+HEADER_SIZE = 128
+BUF = 8192*16
 MAX = 10*1024*1024*1024 # in bytes
 CLEANRATIO = 0.8
 
-GET = 2
-PUT = 3
+GET = 'GET'
+PUT = 'PUT'
+LST = 'LST'
+BYE = 'BYE'
 
 flist = {}
 def init_flist():
@@ -75,72 +77,87 @@ def make_clean(ssig):
 
 class req(SocketServer.StreamRequestHandler):
 	def handle(self):
-		query = self.rfile.read(SIZE).strip().split(',')
-		if len(query) == GET:
-			# get a file from the cache if it exists, else return 0
-			tmp = os.path.join(CACHEDIR, query[0], query[1])
-			fsize = -1
+		while 1:
 			try:
-				fsize = os.stat(tmp).st_size
-			except Exception, e:
-				#print(e)
-				pass
-			else:
-				# cache was useful, update the last access
-				d = os.path.join(CACHEDIR, query[0])
-				os.utime(d, None)
-				flist[query[0]][0] = os.stat(d).st_mtime
-			params = [str(fsize)]
-			self.wfile.write(','.join(params).ljust(SIZE))
-
-			if fsize < 0:
-				#print("file not found in cache %s" % query[0])
-				return
-
-			f = open(tmp, 'rb')
-			cnt = 0
-			while cnt < fsize:
-				r = f.read(BUF)
-				self.wfile.write(r)
-				cnt += len(r)
-
-		elif len(query) == PUT:
-			# add a file to the cache, the tird parameter is the file size
-			(fd, filename) = tempfile.mkstemp(dir=CACHEDIR)
-			try:
-				size = int(query[2])
-				cnt = 0
-				while cnt < size:
-					r = self.rfile.read(BUF)
-					if not r:
-						raise ValueError('Connection closed')
-					os.write(fd, r)
-					cnt += len(r)
-			except Exception, e:
-				#print(e)
-				raise
-			finally:
-				os.close(fd)
-
-			d = os.path.join(CACHEDIR, query[0])
-			try:
-				os.stat(d)
+				self.process_command()
 			except:
-				try:
-					# obvious race condition here
-					os.makedirs(d)
-				except:
-					pass
-			os.rename(filename, os.path.join(d, query[1]))
-			make_clean(query[0])
+				break
+
+	def process_command(self):
+		query = self.rfile.read(HEADER_SIZE).strip().split(',')
+		if query[0] == GET:
+			self.get_file(query[1:])
+		elif query[0] == PUT:
+			self.put_file(query[1:])
+		elif query[0] == LST:
+			self.lst_file(query[1:])
+		elif query[0] == BYE:
+			raise ValueError('exit')
+
+	def get_file(self, query):
+		# get a file from the cache if it exists, else return 0
+		tmp = os.path.join(CACHEDIR, query[0], query[1])
+		fsize = -1
+		try:
+			fsize = os.stat(tmp).st_size
+		except Exception, e:
+			#print(e)
+			pass
 		else:
-			print("invalid query %r" % query)
+			# cache was useful, update the last access
+			d = os.path.join(CACHEDIR, query[0])
+			os.utime(d, None)
+			flist[query[0]][0] = os.stat(d).st_mtime
+		params = [str(fsize)]
+		self.wfile.write(','.join(params).ljust(HEADER_SIZE))
+
+		if fsize < 0:
+			#print("file not found in cache %s" % query[0])
+			return
+
+		f = open(tmp, 'rb')
+		cnt = 0
+		while cnt < fsize:
+			r = f.read(BUF)
+			self.wfile.write(r)
+			cnt += len(r)
+
+	def put_file(self, query):
+		# add a file to the cache, the tird parameter is the file size
+		(fd, filename) = tempfile.mkstemp(dir=CACHEDIR)
+		try:
+			size = int(query[2])
+			cnt = 0
+			while cnt < size:
+				r = self.rfile.read(BUF)
+				if not r:
+					raise ValueError('Connection closed')
+				os.write(fd, r)
+				cnt += len(r)
+		except Exception, e:
+			#print(e)
+			raise
+		finally:
+			os.close(fd)
+
+		d = os.path.join(CACHEDIR, query[0])
+		try:
+			os.stat(d)
+		except:
+			try:
+				# obvious race condition here
+				os.makedirs(d)
+			except:
+				pass
+		os.rename(filename, os.path.join(d, query[1]))
+		make_clean(query[0])
 
 if __name__ == '__main__':
 	init_flist()
 	print("ready (%r dirs)" % len(flist.keys()))
 	SocketServer.ThreadingTCPServer.allow_reuse_address = True
 	server = SocketServer.ThreadingTCPServer(CONN, req)
+	server.timeout = 60 # i think it sounds reasonable
 	server.serve_forever()
 
 
