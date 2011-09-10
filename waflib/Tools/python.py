32f3a8d8ede5260a41e529124c8b64a47be1cf41
tnagy1024 <tnagy1024@f0382ac9-c320-0410-b3f0-b508d59f5a85>
2011-09-10 06:38:30 +0000

    unused imports

--- waflib/Tools/python.py
+++ waflib/Tools/python.py
@@ -19,9 +19,9 @@ Support for Python, detect the headers and libraries and provide
 """
 
 import os, sys
-from waflib import TaskGen, Utils, Utils, Runner, Options, Build, Errors
+from waflib import Utils, Options, Errors
 from waflib.Logs import debug, warn, info, error
-from waflib.TaskGen import extension, taskgen_method, before_method, after_method, feature
+from waflib.TaskGen import extension, before_method, after_method, feature
 from waflib.Configure import conf
 
 FRAG = '''
