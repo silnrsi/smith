42b9a5c6c47d7e41972647e7a8b7853cae3a28b0
Nicolas Spalinger <nicolas_spalinger@sil.org>
2015-04-28 19:14:23 +0100

    Fixed metadata logic.

--- docs/smith/build-docs.sh
+++ docs/smith/build-docs.sh
@@ -2,17 +2,15 @@
 
 # Metadata of the smith source tree
 revnumber=`hg tip | grep "changeset" | cut -d : -f 2-`
-revremark=`hg tip | grep -A2 "description" | cut -d : -f 2-`
 revdate=`hg tip | grep "date" | cut -d : -f 2-`
-revauthor=`hg tip | grep "user" | cut -d : -f 2-`
-revrelease=`hg tags | tail -n1`
+revremark=`hg tip | grep -A2 "description" | cut -d : -f 2-`
+revrelease=`hg tags | sed -n '2p'`
 
 # Make and install the manual
 echo revnumber: $revnumber
 echo revremark: $revremark
 echo revdate: $revdate 
-echo revauthor: $revauthor
 echo revrelease: $revrelease
 
-a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revnumber="Current revision: $revnumber" -a revremark="Last commit log:'$revremark'. Current realease: $revrelease" -a revdate="$revdate" -a author="revauthor"  manual.txt
+a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revnumber="release: $revrelease" -a revdate="$revdate" -a revremark="last commit log: $revremark"  -a author="$revauthour" manual.txt
 a2x -f xhtml manual.txt
