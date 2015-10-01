ab6dd40595d75990b0314e633aee34ede7f055b0
Bobby de Vos <bobby_devos@sil.org>
2015-10-01 17:43:58 -0600

    Update manual due to the move to GitHub
    Updates were needed for both the
    * build process of the manual
    * contents of the manual

--- docs/smith/build-docs.sh
+++ docs/smith/build-docs.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 # Metadata of the smith source tree
-revnumber=`hg tip | grep "changeset" | cut -d : -f 2-`
-revdate=`hg tip | grep "date" | cut -d : -f 2-`
-revremark=`hg tip | grep -A2 "description" | cut -d : -f 2-`
-revrelease=`hg tags | sed -n '2p'`
+revdate=$(git log -n 1 | grep "Date" | cut -d : -f 2-)
+revauthor=$(git log -n 1 | grep "Author" | cut -d : -f 2-)
 
 # Make and install the manual
-echo revnumber: $revnumber
-echo revremark: $revremark
 echo revdate: $revdate 
-echo revrelease: $revrelease
+echo revauthor: $revauthor
 
-a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revnumber="release: $revrelease" -a revdate="$revdate" -a revremark="last commit log: $revremark"  -a author="$revauthour" manual.txt
+a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" -a author="$revauthor" manual.txt
 a2x -f xhtml manual.txt
