f6b01827047c4b5a824e2a65f9366df61ab954c3
Bobby de Vos <bobby_devos@sil.org>
2016-01-19 17:37:56 -0600

    Use simpler metadata
    Launchpad does not have access to git metadata,
    so the manual that Launchpad built had no metadata.

--- docs/smith/build-docs.sh
+++ docs/smith/build-docs.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Metadata of the smith source tree
-revdate=$(git log -n 1 | grep "Date" | cut -d : -f 2-)
-revauthor=$(git log -n 1 | grep "Author" | cut -d : -f 2-)
-
-# Make and install the manual
+revdate=$(date)
 echo revdate: $revdate 
-echo revauthor: $revauthor
 
-a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" -a author="$revauthor" manual.txt
+a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" manual.txt
 a2x -f xhtml manual.txt
