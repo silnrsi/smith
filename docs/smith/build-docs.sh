22237fad8a95d421549938da90da6d837c513847
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-02-03 10:18:24 +0100

    Added verbosity and epub output.

--- docs/smith/build-docs.sh
+++ docs/smith/build-docs.sh
@@ -3,5 +3,6 @@
 revdate=$(date)
 echo revdate: $revdate 
 
-a2x -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" manual.asc
-a2x -f xhtml manual.asc
+a2x -v -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" manual.asc
+a2x -v -f xhtml manual.asc
+a2x -v -f epub -d book manual.asc 
