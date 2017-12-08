771d7bffb153cd39d87286582f4d6e3766cb472b
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-12-08 16:22:44 -0600

    Added pep8 and pylint-suggested changes.

--- smith_dev.py
+++ smith_dev.py
@@ -3,27 +3,27 @@
 # Launch smith from its source repo.
 # The main use is for smith development.
 #
-# The usual smith script unpacks its tarball and 
+# The usual smith script unpacks its tarball and
 # imports code from the unpack location.
 # This script should run exactly as smith is run.
 # E.g. From a folder with a wscript, run:
 #  "python <smith repo path>/smith_dev.py configure"
 # Launching tested under Linux and Win10
 
-smith_ver = '1.6.8' # !! MUST match WAFVERSION value in waflib/Context.py !!
+smith_ver = '1.6.8'  # !! MUST match WAFVERSION value in waflib/Context.py !!
 
-#smith_repo is location of waf modules (-> Context.waf_dir)
-#cwd is used to find wscript (-> Context.launch_dir)
+# smith_repo is location of waf modules (-> Context.waf_dir)
+# cwd is used to find wscript (-> Context.launch_dir)
 import sys, os
 smith_repo_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 cwd = os.getcwd()
-#print("cwd = %s\nsmith_repo = %s\n" % (cwd, smith_repo))
+# print("cwd = %s\nsmith_repo = %s\n" % (cwd, smith_repo))
 
-#smithlib and waflib should be under the folder where this script is launched
+# smithlib and waflib should be under the folder where this script is launched
 # smithlib/__init.py__ is not required by the usual smith script
 # because smithlib content is copied to waflib/extras
 try:
-    from smithlib import wsiwaf #works with local smithlib/__init.py__ file
+    from smithlib import wsiwaf  # works with local smithlib/__init.py__ file
 except:
     # add smithlib to the Python path (though it's not a package)
     sys.path.insert(1, os.path.join(smith_repo_dir, 'smithlib'))
