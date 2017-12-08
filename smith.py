6c1c0e3057e1ae7a441204064c0ea8cf6ff14c84
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-12-08 16:20:34 -0600

    Added pep8 and pylint-suggested changes.

--- smith.py
+++ smith.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 
-import sys, os
+import sys
+import os
 
 from smithlib import wsiwaf
 from waflib import Scripting, Context
 
-#assumes smith.py runs in folder above waflib
+# assumes smith.py runs in folder above waflib
 # this fails if the script is relocated somewhere else by installation
 # or if it is executed by launcher (since the launcher path will be found)
 # but that likely doesn't matter since smith wscripts don't usually
 # have items in them requiring wafdir be set correctly
-#waf_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+# waf_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 waflib_dir = os.path.dirname(os.path.abspath(Scripting.__file__)).split(os.sep)
 waf_dir = os.sep.join(waflib_dir[:-1])
 
+
 def main():
     Scripting.waf_entry_point(os.getcwd(), Context.WAFVERSION, waf_dir)
+
 
 if __name__ == "__main__":
     sys.exit(main())
