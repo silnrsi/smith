b40853a6033614668ffa37580745cd68e43b2dce
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-12-14 09:41:16 -0600

    Added more fields to flesh out metadata.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 'Setuptools installation file'
 __url__ = 'http://github.com/silnrsi/smith'
 __copyright__ = 'Copyright (c) 2017 SIL International (http://www.sil.org)'
+__author__ = 'Martin Hosken'
+__license__ = 'Released under the 3-Clause BSD License (http://opensource.org/licenses/BSD-3-Clause)'
 __version__ = '0.3.5'
 
 try:
@@ -15,6 +17,9 @@ setup(
     name='smith',
     version=__version__,
     url=__url__,
+    copyright=__copyright__,
+    license=__license__,
+    author=__author__,
     description='smith build tool',
     long_description='''smith is a Python-based framework for building, testing
     and maintaining WSI (Writing Systems Implementation) components such as
@@ -32,5 +37,6 @@ setup(
         'console_scripts': [
             'smith=smith:main',
         ],
-    }
+    },
+    platforms=["Any"]
 )
