5cdaaf9bec870cff77d752028cfaac6e80133700
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-12-20 20:48:24 -0600

    Streamlined provision: shallow clones, version of vagrant/virtualbox, reducing uneeded packages.

--- vm-install/provision.sh
+++ vm-install/provision.sh
@@ -2,6 +2,8 @@
 # A provisioning script for Vagrant to make it easier to get the latest smith & friends from the PPAs and/or source.
 # This is designed to be called by the Vagrantfile wich expects this file to be in the same directory by default.
 
+# This is for Ubuntu xenial/16.04 LTS
+
 # configure things for non-interactive and sdtin
 ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
 dpkg-reconfigure debconf -f noninteractive -p critical
@@ -35,11 +37,17 @@ apt-get upgrade -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="-
 # checking if we already have local checkouts 
 if [ -d /usr/local/builds/ ]
 then
-    echo "you already have previous builds, it's easier to delete them and start afresh."
+    echo " "
+    echo "You already have previous builds, it's easier to delete them and start afresh. "
+    echo " "
+    echo "Deleting /usr/local/builds... "
+    echo " "
     rm -rf /usr/local/builds
 fi
 
 mkdir -p /usr/local/builds
+
+chmod -R 766 /usr/local/builds 
 
 # graphite
 echo " "
@@ -48,10 +56,10 @@ echo "Installing graphite from source"
 echo " "
 echo " "
 
-apt-get install asciidoc autotools-dev bsdmainutils build-essential cmake cmake-data cpp cpp-5 debhelper dh-exec dh-strip-nondeterminism docbook-xml docbook-xsl doxygen dpkg-dev fontconfig fontconfig-config fonts-dejavu-core g++ gcc gettext graphviz groff-base intltool-debian libarchive-zip-perl libarchive13 libasan2 libatomic1 libavahi-client3 libavahi-common-data libavahi-common3 libblas-common libblas3 libc-dev-bin libc6-dev libcairo2 libcc1-0 libcdt5 libcgraph6 libcilkrts5 libclang1-3.6 libcroco3 libcups2 libcupsfilters1 libcupsimage2 libcurl3 libdatrie1 libfile-stripnondeterminism-perl libfontconfig1 libgcc-5-dev libgd3 libgfortran3 libgomp1 libgs9 libgs9-common libgvc6 libgvpr2 libice6 libijs-0.35 libisl15 libitm1 libjbig0 libjbig2dec0 libjpeg-turbo8 libjpeg8 libjsoncpp1 libkpathsea6 liblapack3 liblcms2-2 libllvm3.6v5 liblsan0 libltdl7 libmpc3 libmpx0 libobjc-5-dev libobjc4 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpaper-utils libpaper1 libpathplan4 libpipeline1 libpixman-1-0 libpoppler58 libpotrace0 libptexenc1 libquadmath0 libsm6 libstdc++-5-dev libsynctex1 libtexlua52 libtexluajit2 libthai-data libthai0 libtiff5 libtimedate-perl libtsan0 libubsan0 libunistring0 libvpx3 libx11-6 libx11-data libxau6 libxaw7 libxcb-render0 libxcb-shm0 libxcb1 libxdmcp6 libxext6 libxi6 libxml2-utils libxmu6 libxpm4 libxrender1 libxslt1.1 libxt6 libzzip-0-13 linux-libc-dev make man-db po-debconf poppler-data python-apt python-numpy sgml-data t1utils x11-common xdg-utils xsltproc libfreetype6-dev libfreetype6 texlive-xetex texlive-latex-extra xdg-utils xsltproc -y -q
+apt-get install asciidoc autotools-dev bsdmainutils build-essential cmake cmake-data cpp cpp-5 debhelper dh-exec dh-strip-nondeterminism docbook-xml docbook-xsl doxygen dpkg-dev fontconfig fontconfig-config fonts-dejavu-core g++ gcc gettext graphviz groff-base intltool-debian libarchive-zip-perl libarchive13 libasan2 libatomic1 libavahi-client3 libavahi-common-data libavahi-common3 libblas-common libblas3 libc-dev-bin libc6-dev libcairo2 libcc1-0 libcdt5 libcgraph6 libcilkrts5 libclang1-3.6 libcroco3 libcups2 libcupsfilters1 libcupsimage2 libcurl3 libdatrie1 libfile-stripnondeterminism-perl libfontconfig1 libgcc-5-dev libgd3 libgfortran3 libgomp1 libgs9 libgs9-common libgvc6 libgvpr2 libice6 libijs-0.35 libisl15 libitm1 libjbig0 libjbig2dec0 libjpeg-turbo8 libjpeg8 libjsoncpp1 libkpathsea6 liblapack3 liblcms2-2 libllvm3.6v5 liblsan0 libltdl7 libmpc3 libmpx0 libobjc-5-dev libobjc4 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpaper-utils libpaper1 libpathplan4 libpipeline1 libpixman-1-0 libpoppler58 libpotrace0 libptexenc1 libquadmath0 libsm6 libstdc++-5-dev libsynctex1 libtexlua52 libtexluajit2 libthai-data libthai0 libtiff5 libtimedate-perl libtsan0 libubsan0 libunistring0 libvpx3 libx11-6 libx11-data libxau6 libxaw7 libxcb-render0 libxcb-shm0 libxcb1 libxdmcp6 libxext6 libxi6 libxml2-utils libxmu6 libxpm4 libxrender1 libxslt1.1 libxt6 libzzip-0-13 linux-libc-dev make man-db po-debconf poppler-data python-apt python-numpy sgml-data t1utils x11-common xdg-utils xsltproc libfreetype6-dev libfreetype6 texlive-xetex texlive-latex-extra -y -q
 
 cd /usr/local/builds
-git clone https://github.com/silnrsi/graphite.git
+git clone --depth 1 https://github.com/silnrsi/graphite.git
 cd graphite
 mkdir build
 cd build
@@ -68,7 +76,7 @@ echo " "
 apt-get install build-essential cmake gcc g++ libfreetype6-dev libglib2.0-dev libcairo2-dev automake libtool pkg-config ragel gtk-doc-tools -y -q
 apt-get install libgirepository1.0-dev python-gi -y -q
 cd /usr/local/builds
-git clone https://github.com/harfbuzz/harfbuzz.git
+git clone --depth 1 https://github.com/harfbuzz/harfbuzz.git
 cd harfbuzz
 ./autogen.sh --with-graphite2 --with-gobject --enable-introspection --with-ucdn=no
 make
@@ -83,9 +91,9 @@ echo " "
 echo "Installing ots from source"
 echo " "
 echo " "
-apt-get install zlib1g-dev -y -q
+apt-get install build-essential autoconf automake pkg-config zlib1g-dev -y -q
 cd /usr/local/builds
-git clone --recursive https://github.com/khaledhosny/ots.git
+git clone --depth 1 --recursive https://github.com/khaledhosny/ots.git
 cd ots
 ./autogen.sh
 ./configure --enable-debug --enable-graphite
@@ -116,7 +124,7 @@ echo " "
 echo " "
 apt-get install mono-mcs libmono-corlib4.5-cil libmono-system-windows-forms4.0-cil libmono-system-web4.0-cil xsltproc xdg-utils -y -q 
 cd /usr/local/builds
-git clone https://github.com/HinTak/Font-Validator.git fontval
+git clone --depth 1 https://github.com/HinTak/Font-Validator.git fontval
 cd fontval
 make
 make gendoc
@@ -175,24 +183,30 @@ echo " "
 echo "Smith: "
 apt-cache show smith | grep Version: | grep snapshot
 
-echo "with: "
+echo "with fontforge version : "
 apt-cache show fontforge | grep Version: 
+echo "with fontttols version : "
+apt-cache show fonttools | grep Version: 
 hb-view --version
 xetex --version | grep Live 
 echo " "
 echo " "
 
-echo "To go to the shared folder to run smith commands on your project, type:" 
+echo "To go to the shared folder to run smith commands on your project(s), type:" 
 echo " "
 echo "vagrant ssh"
 echo "cd /smith"
-echo "cd <folder of my project>"
+echo "cd <folder of my project(s)>"
 echo "smith distclean"
 echo "smith configure"
 echo "smith build"
 echo "smith alltests"
 echo "smith pdf"
 echo "smith zip"
+echo "smith tarball"
+echo "smith release"
+echo "for more details type: man smith"
+echo "or refer to the smith manual"
 echo " "
 
 
