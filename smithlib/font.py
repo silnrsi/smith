d864b0ded7aadfea1a1a13d3ca9123b0a06a53b6
Nicolas Spalinger <nicolas.spalinger@sil.org>
2018-05-10 16:02:02 +0200

    Update copyright year.

--- smithlib/font.py
+++ smithlib/font.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 ''' font module '''
 __url__ = 'http://github.com/silnrsi/smith'
-__copyright__ = 'Copyright (c) 2011 SIL International (http://www.sil.org)'
+__copyright__ = 'Copyright (c) 2011-2018 SIL International (http://www.sil.org)'
 __author__ = 'Martin Hosken'
 __license__ = 'Released under the 3-Clause BSD License (http://opensource.org/licenses/BSD-3-Clause)'
 
