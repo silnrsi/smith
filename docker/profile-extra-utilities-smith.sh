0ebc273626c1410bbe44a99ba77a39923449fac5
Nicolas Spalinger <nicolas.spalinger@sil.org>
2023-01-29 18:03:05 -0600

    Fixed typo.

--- docker/profile-extra-utilities-smith.sh
+++ docker/profile-extra-utilities-smith.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 
-# extend PATH to include standard folder container extra utilities to run within the smith toolchain environment
+# extend PATH to include standard folder containing extra utilities to run within the smith toolchain environment
 if [ -d "/smith/bin" ] ; then 
 		PATH="/smith/bin:$PATH"
 fi
