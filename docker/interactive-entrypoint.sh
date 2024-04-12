b7a31c1dd784deb7334ec045e78845a015054429
Nicolas Spalinger <nicolas.spalinger@sil.org>
2024-04-11 22:05:07 -0500

    Tweaked welcome message, included distro codename and release number for testing

--- docker/interactive-entrypoint.sh
+++ docker/interactive-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
+
 stat_gid=$(stat --printf '%g' /smith)
 stat_uid=$(stat --printf '%u' /smith)
 groupadd -f -g ${BUILDER_GID:=${stat_gid/#0/1000}} builder
 useradd -m builder -u ${BUILDER_UID:=${stat_uid/#0/1000}} -g $BUILDER_GID
 CMND="$@"
-echo Welcome to the smith interactive font build system.
+release=$(grep "RELEASE" /etc/lsb-release | cut -d "=" -f2 )
+codename=$(grep "CODENAME" /etc/lsb-release | cut -d "=" -f2)
+echo "Welcome to smith: font development, testing and release ($codename - $release)".
 cd /smith
 exec runuser builder --pty --command="exec $CMND"
