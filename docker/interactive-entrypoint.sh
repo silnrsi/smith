0f19b88bd97390be56047220786f02445fadea17
Nicolas Spalinger <nicolas.spalinger@sil.org>
2024-04-28 19:55:47 -0500

    Added workaround for uid uniqueness

--- docker/interactive-entrypoint.sh
+++ docker/interactive-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 stat_gid=$(stat --printf '%g' /smith)
 stat_uid=$(stat --printf '%u' /smith)
 groupadd -f -g ${BUILDER_GID:=${stat_gid/#0/1000}} builder
-useradd -m builder -u ${BUILDER_UID:=${stat_uid/#0/1000}} -g $BUILDER_GID
+useradd -m builder -o -u ${BUILDER_UID:=${stat_uid/#0/1000}} -g $BUILDER_GID
 CMND="$@"
 release=$(grep "RELEASE" /etc/lsb-release | cut -d "=" -f2 )
 codename=$(grep "CODENAME" /etc/lsb-release | cut -d "=" -f2)
