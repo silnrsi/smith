f9e43194e14153885515ffc7caef1a0af9aa3ce5
Nicolas Spalinger <nicolas.spalinger@sil.org>
2024-08-06 22:30:05 -0500

    Changes configuration to focus on the html output and no longer bother with other formats

--- docs/smith/build-docs.sh
+++ docs/smith/build-docs.sh
@@ -3,6 +3,6 @@
 revdate=$(date)
 echo revdate: $revdate 
 
-a2x -v -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" manual.asc
+#a2x -v -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" manual.asc
 a2x -v -f xhtml manual.asc
-a2x -v -f epub -d book manual.asc 
+#a2x -v -f epub -d book manual.asc 
