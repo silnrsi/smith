b347707a82b4ec09d17685ebb22b428eef2d53e1
Nicolas Spalinger <nicolas.spalinger@sil.org>
2024-08-09 16:45:51 -0500

    Simplifed mention of last update.

--- docs/smith/build-docs.sh
+++ docs/smith/build-docs.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-revdate=$(date)
-echo revdate: $revdate 
-
-#a2x -v -f pdf -L --dblatex-opts=" -s asciidoc-dblatex.sty" -a revdate="$revdate" manual.asc
 a2x -v -f xhtml manual.asc
-#a2x -v -f epub -d book manual.asc 
