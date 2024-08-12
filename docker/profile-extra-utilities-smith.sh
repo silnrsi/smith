b01e9070df8091473ed2720d38f10603276a3324
n7s <n7s@users.noreply.github.com>
2024-08-12 23:48:17 +0000

    deploy: 4e7543d5a26d5f75b42b3b524de36464ee24c9bf

--- docker/profile-extra-utilities-smith.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck shell=sh
-
-# extend PATH to include standard folder containing extra utilities to run within the smith toolchain environment
-if [ -d "/smith/bin" ] ; then 
-		PATH="/smith/bin:$PATH"
-fi
--- src/waflib/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-# encoding: utf-8
-# Thomas Nagy, 2005-2010 (ita)
