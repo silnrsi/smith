001d4b1280685273ab0fd3643462cc1ab65e2e42
n7s <n7s@users.noreply.github.com>
2024-08-13 01:50:05 +0000

    deploy: 753c6729cd02b1970b1404257d5ebd4851146c52

--- build-docs.sh
+++ build-docs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#a2x -v -f xhtml manual.asc
-asciidoctor -t -v -b xhtml5 -n -a toc=left -a source-highlighter=pygments manual.adoc
+asciidoctor -t -v -b xhtml5 -n -a toc=left -a source-highlighter=rouge manual.adoc
 
 
